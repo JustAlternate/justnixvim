6893029cbf97af38a2a9a25fe18949cb0912f958
JustAlternate <loicw@justalternate.fr>
2025-02-16 13:11:15 +0100

    fix: temp deactivate go-nvim

--- config/plugins/go-nvim.nix
+++ config/plugins/go-nvim.nix
@@ -2,15 +2,15 @@
 {
   extraPlugins = with pkgs.vimPlugins; [
     go-nvim
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "guihua";
-      src = pkgs.fetchFromGitHub {
-        owner = "ray-x";
-        repo = "guihua.lua";
-        rev = "d783191eaa75215beae0c80319fcce5e6b3beeda";
-        hash = "sha256-XpUsbj1boDfbyE8C6SdOvZdkd97682VVC81fvQ5WA/4=";
-      };
-    })
+    # (pkgs.vimUtils.buildVimPlugin {
+    #   name = "guihua";
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "ray-x";
+    #     repo = "guihua.lua";
+    #     rev = "d783191eaa75215beae0c80319fcce5e6b3beeda";
+    #     hash = "sha256-XpUsbj1boDfbyE8C6SdOvZdkd97682VVC81fvQ5WA/4=";
+    #   };
+    # })
   ];
   extraConfigLua = ''
     require('go').setup({
