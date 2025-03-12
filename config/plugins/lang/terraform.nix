49f34ea1587e6aab6f2f46deabac0517521dcc2e
JustAlternate <loicw@justalternate.fr>
2025-03-12 21:52:38 +0100

    fix: commenting out tflint cause its borken or idk how it work lol

--- config/plugins/lang/terraform.nix
+++ config/plugins/lang/terraform.nix
@@ -5,16 +5,16 @@
       terraform_lsp.enable = true;
     };
 
-    lint = {
-      lintersByFt = {
-        terraform = [ "tflint" ];
-      };
-
-      linters = {
-        tflint = {
-          cmd = "${pkgs.tflint}/bin/tflint";
-        };
-      };
-    };
+    # lint = {
+    #   lintersByFt = {
+    #     terraform = [ "tflint" ];
+    #   };
+    #
+    #   linters = {
+    #     tflint = {
+    #       cmd = "${pkgs.tflint}/bin/tflint";
+    #     };
+    #   };
+    # };
   };
 }
