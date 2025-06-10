1ada910e1a750522929620be69e5805ae707fdc9
JustAlternateIDZ <loic.weber@iadvize.com>
2025-06-10 09:23:41 +0200

    feat: aw-watcher nvim

---
+++ config/plugins/aw-watcher.nix
@@ -0,0 +1,12 @@
+{
+  plugins.aw-watcher = {
+    enable = true;
+    settings = {
+      aw_server = {
+        host = "127.0.0.1";
+        port = 5600;
+        pulsetime = 30;
+      };
+    };
+  };
+}
--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./alpha.nix
     ./lang
+    ./aw-watcher.nix
     ./treesitter/treesitter.nix
     ./treesitter/treesitter-textobjects.nix
     ./cmp.nix
