7285839151898431dcb7c101ae2d93b0c31777fb
JustAlternateIDZ <loic.weber@iadvize.com>
2025-06-12 13:39:22 +0200

    feat: working guihua for nvim-go

--- config/plugins/lang/go.nix
+++ config/plugins/lang/go.nix
@@ -33,22 +33,28 @@
       };
     };
   };
-
   extraPlugins = with pkgs.vimPlugins; [
     go-nvim
-    # (pkgs.vimUtils.buildVimPlugin {
-    #   name = "guihua";
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "ray-x";
-    #     repo = "guihua.lua";
-    #     rev = "87bea7b98429405caf2a0ce4d029b027bb017c70";
-    #     hash = "sha256-R/ckeCwzWixvL7q2+brvqcvfSK9Mx8pu6zOFgh2lde4=";
-    #   };
-    # })
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "guihua";
+      src = pkgs.fetchFromGitHub {
+        owner = "ray-x";
+        repo = "guihua.lua";
+        rev = "87bea7b98429405caf2a0ce4d029b027bb017c70";
+        hash = "sha256-R/ckeCwzWixvL7q2+brvqcvfSK9Mx8pu6zOFgh2lde4=";
+      };
+      buildPhase = ''
+        (
+          cd lua/fzy
+          make
+        )
+      '';
+      nvimSkipModules = [ "fzy.fzy-lua-native" ];
+    })
   ];
   extraConfigLua = ''
     require('go').setup({
-      run_in_floaterm = false
+      run_in_floaterm = true
     })
   '';
   keymaps = [
@@ -62,19 +68,25 @@
       mode = "n";
       key = "<leader>cgtf";
       action = "<cmd>GoTestFile<cr>";
+      options.desc = "GoTestFile";
+    }
+    {
+      mode = "n";
+      key = "<leader>cgtt";
+      action = "<cmd>GoTest<cr>";
       options.desc = "GoTest";
     }
     {
       mode = "n";
       key = "<leader>cgtc";
       action = "<cmd>GoTermClose<cr>";
-      options.desc = "GoTest";
+      options.desc = "GoTermClose";
     }
     {
       mode = "n";
       key = "<leader>cgf";
       action = "<cmd>GoFmt<cr>";
-      options.desc = "GoTest";
+      options.desc = "GoFmt";
     }
   ];
 
