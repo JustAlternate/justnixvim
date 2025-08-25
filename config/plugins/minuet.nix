00d03e62377febebe6064481a1042f6a8f2071f7
JustAlternateIDZ <loic.weber@iadvize.com>
2025-08-25 14:07:07 +0200

    fix: minuet generate completion on keystroke

--- config/plugins/minuet.nix
+++ config/plugins/minuet.nix
@@ -11,10 +11,10 @@
         -- context before cursor will be used. This option should be between 0 and
         -- 1, context_ratio = 0.75 means the ratio will be 3:1.
         context_ratio = 0.75,
-        request_timeout = 5,
+        request_timeout = 3,
         throttle = 750, -- Increase to reduce costs and avoid rate limits
         debounce = 500, -- Increase to reduce costs and avoid rate limits
-        n_completions = 1,
+        n_completions = 3,
         provider_options = {
             openai_compatible = {
                 stream = true,
@@ -22,20 +22,26 @@
                 api_key = 'OPENROUTER_API_KEY',
                 model = 'mistralai/codestral-2508',
                 optional = {
-                    max_tokens = 4000,
+                    max_tokens = 2500,
                     top_p = 0.9,
                 },
             },
         },
         cmp = {
-            enable_auto_complete = true,
+            enable_auto_complete = false,
         },
         blink = {
             enable_auto_complete = false,
         },
+        lsp   = {
+          enabled_ft = {},                 -- don't attach as LSP unless you want to
+          enabled_auto_trigger_ft = {},    -- definitely no auto-trigger
+          warn_on_blink_or_cmp = false,    -- optional: silence the hint
+        },
         virtualtext = {
-            auto_trigger_ft = { "*" },
-            show_on_completion_menu = true,
+            auto_trigger_ft = {},
+            show_on_completion_menu = false,
+          
             keymap = {
                 -- accept whole completion
                 accept = '<D-a>',
@@ -45,10 +51,10 @@
                 -- e.g. "A-z 2 CR" will accept 2 lines
                 accept_n_lines = '<A-z>',
                 -- Cycle to prev completion item, or manually invoke completion
-                prev = '<A-[>',
+                prev = '<D-e>',
                 -- Cycle to next completion item, or manually invoke completion
-                next = '<A-]>',
-                dismiss = '<A-e>',
+                next = '<D-z>',
+                dismiss = '<D-r>',
             },
         },
     }
