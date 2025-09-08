5dad8853927a7dfcaf80320cebe264a499829368
JustAlternateIDZ <loic.weber@iadvize.com>
2025-09-08 16:24:21 +0200

    fix: codecompanion using openrouter

--- config/plugins/codecompanion.nix
+++ config/plugins/codecompanion.nix
@@ -2,14 +2,26 @@
   plugins.codecompanion = {
     enable = true;
     settings = {
+      strategies = {
+        agent = {
+          adapter = "openrouter";
+        };
+        chat = {
+          adapter = "openrouter";
+        };
+        inline = {
+          adapter = "openrouter";
+        };
+      };
       adapters = {
-        openrouter_ai = {
+        openrouter = {
           __raw = ''
             function()
               return require("codecompanion.adapters").extend("openai_compatible", {
                 env = {
                   url="https://openrouter.ai/api",
                   chat_url = "/v1/chat/completions",
+                  api_key = "OPENROUTER_API_KEY",
                 };
                 schema = {
                   model = {
